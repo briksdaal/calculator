f79e79afce7c7781a0f6149e6ed3c2ab113a4320
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-01-10 04:21:23 +0200

    Create math functions

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-para = document.querySelector("p");
-para.textContent = "Test is working";
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return "Error";
+  };
+  return a / b;
+}
